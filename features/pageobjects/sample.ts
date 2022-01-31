1c27a84a42b54e76743e12294f30bc6fdedfb0ba
Sivakumar960 <81681201+Sivakumar960@users.noreply.github.com>
2022-01-31 14:52:06 +0530

    Update sample.ts

--- features/pageobjects/sample.ts
+++ features/pageobjects/sample.ts
@@ -4,16 +4,19 @@ import report from '@wdio/allure-reporter'
 export const providetext=async(element:ChainablePromiseElement<Promise<WebdriverIO.Element>>,text)=>{
     await element.setValue(text)
     await report.addStep(`User entered ${text}`)
+    await browser.takeScreenshot()
 }
 
 
 export const providingclick=async(elem:ChainablePromiseElement<Promise<WebdriverIO.Element>>)=>{
     await elem.click()
     await report.addStep(`user clicked on ${elem.selector}`)
+    await browser.takeScreenshot()
 }
 
 export const havingtext=async(element:ChainablePromiseElement<Promise<WebdriverIO.Element>>,expectedtext)=>{
     await expect(element).toHaveText(expectedtext)
     report.addStep(`checking ${element.selector} with ${expectedtext} `)
+    await browser.takeScreenshot()
 
 }
